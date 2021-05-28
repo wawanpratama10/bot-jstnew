9530b54f7ddf193d9bcdf32ed2341648664073ba
wawanpratama10 <84949517+wawanpratama10@users.noreply.github.com>
2021-05-28 17:18:12 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,51 +16,47 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
+        click  /predict to know about i and v`
     );   
 });
 
-bot.onText(/\/menu/, (msg) => { 
-    console.log(msg)
-    bot.sendMessage(
-        msg.chat.id,
-        `this is your main menu`
-    );   
-});
+
 
 state = 0;
 bot.onText(/\/predict/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
-        `masukan nilai i|v contohnya 9|9`
-    );  
+        `input nilai i|v example 4|3`
+    );   
     state = 1;
 });
 
-bot.on('message'), (msg) => {
+bot.on('message', (msg) => {
     if(state == 1){
-        console.log(msg.text);
         s = msg.text.split("|");
         i = s[0]
         v = s[1]
         model.predict(
-        [
-            parseFloat(s[0]), // string to float
-            parseFloat(s[1])
-        ]
-    ).then((jres)=>{
-        bot.sendMessage(
-            msg.chat.id,
-            `nilai v yang diprediksi adalah $(jres[0]} volt`
-        );   
-        bot.sendMessage(
-            msg.chat.id,
-            `nilai v yang diprediksi adalah $(jres[1]} volt`
+            [
+                parseFloat(s[0]), // string to float
+                parseFloat(s[1])
+            ]
+        ).then((jres)=>{
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai v yang diprediksi adalah ${jres[0]} volt`
+            );   
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai p yang diprediksi adalah ${jres[1]} watt`
     );   
-    })
+})
     }else{
         state = 0
+    }
 })
+
+
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
