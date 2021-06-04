53e7813309097a9e9e7533eb7c2c0d5b1fb4ed09
wawanpratama10 <84949517+wawanpratama10@users.noreply.github.com>
2021-06-04 21:48:39 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -37,7 +37,7 @@ async function classify(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/wawanpratama10/bot-jstnew/main/public/cls_model/model.json';
+        const path = 'https://raw.githubusercontent.com/Farisfold/bot-jst_salman/main/public/cls_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
